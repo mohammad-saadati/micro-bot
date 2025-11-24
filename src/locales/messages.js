723a764cc43e6b1b2d61186ca8b61f6684883d58
Mohammad Saadati <32834340+mohammad-saadati@users.noreply.github.com>
2025-11-24 21:01:25 +0330

    feat: translations

---
+++ src/locales/messages.js
@@ -0,0 +1,14 @@
+const translations = {
+  google: "گوگل",
+  microsoft: "مایکروسافت",
+  farazin: "فرازین",
+  selectLanguage: "زبان ترجمه را انتخاب کنید",
+  googleTranslate: "ترجمه با گوگل",
+  microsoftTranslate: "ترجمه با مایکروسافت",
+  farazinTranslate: "ترجمه با فرازین",
+  fa: "فارسی",
+  en: "انگلیسی",
+  sendWord: "کلمه یا عبارت مورد نظر خود را ارسال کنید",
+};
+
+module.exports = { translations };
